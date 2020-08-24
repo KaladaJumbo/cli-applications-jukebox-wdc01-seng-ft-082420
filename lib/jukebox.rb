b6cd9f0787f990fe4d0ac4d6cc8948d691917209
lively-system-6174 <wkalada1@gmail.com>
2020-08-24 05:47:20 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -73,7 +73,7 @@ def play(songs)
     end
 
     if  songs[user_responce.to_i].nil? != nil && songs[user_responce.to_i] != 0
-        puts "Playing " + songs[user_responce.to_i]
+        puts "Playing #{songs[user_responce.to_i]}"
     elsif songs.include?(user_responce) 
         puts "Playing #{user_responce}"
     else
