2e522549b2491f56652e75fc1e6562d1fc05a7b1
lively-system-6174 <wkalada1@gmail.com>
2020-08-24 05:24:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,7 +38,7 @@ def help
 
     puts    "I accept the following commands:"
     puts    "- help : displays this help message"
-    puts    "- list : displays a list of sons you can play"
+    puts    "- list : displays a list of songs you can play"
     puts    "- play : lets you chooce a song to play"
     puts    "- exit : exits this program"
 
